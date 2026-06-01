ddb56d2388b75af723a970853ceb1ee2e3631f01
50084663 <50084663@est.ort.edu.ar>
2026-06-01 10:41:29 -0300

    .

---
+++ Assets/Scripts/ConoPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConoPickup : MonoBehaviour
+
+{
+    public static int conosRecolectados = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("¡Encontraste el cono escondido! ¡GANASTE!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ContadorUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContadorUI : MonoBehaviour
+{
+    public Text textoContador;
+
+    void Update()
+    {
+        textoContador.text = "Conos: " + ConoPickup.conosRecolectados;
+    }
+}
